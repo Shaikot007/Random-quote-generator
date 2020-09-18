b0527db8a11e9e2795ac6534635c510ab226d621
Shaikot007 <shaikot999@gmail.com>
2020-09-18 22:11:15 +0600

    Updates

---
+++ precache-manifest.6cce4121cb158a6a682bfc7e6ea9c4d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c129bd79ca1696fcf6a29c71e8f07e",
+    "url": "/Random-quote-generator/index.html"
+  },
+  {
+    "revision": "66cfeeb76da875eeddec",
+    "url": "/Random-quote-generator/static/css/main.965a2416.chunk.css"
+  },
+  {
+    "revision": "525e4e8c6589afee802d",
+    "url": "/Random-quote-generator/static/js/2.e930acd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-quote-generator/static/js/2.e930acd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66cfeeb76da875eeddec",
+    "url": "/Random-quote-generator/static/js/main.8e4839f6.chunk.js"
+  },
+  {
+    "revision": "16c680bcd3d90090b850",
+    "url": "/Random-quote-generator/static/js/runtime-main.98d988f4.js"
+  }
+]);
